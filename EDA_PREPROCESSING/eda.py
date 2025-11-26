d7810dd6868c8d705b7770afd668289520916ca3
Mateusz Stasiak <01189052@pw.edu.pl>
2025-11-26 20:54:45 +0100

    test 2

--- EDA_PREPROCESSING/eda.py
+++
@@ -1,8 +0,0 @@
-import os
-import numpy as np
-import pandas as pd
-
-BASE_DIR= os.path.dirname(os.path.abspath(__file__))
-PROJECT_ROOT_DIR= os.path.abspath(os.path.join(BASE_DIR,".."))
-
-DATABASE_DIR= os.path.abspath(os.path.join(PROJECT_ROOT_DIR,"PTB-XL"))
