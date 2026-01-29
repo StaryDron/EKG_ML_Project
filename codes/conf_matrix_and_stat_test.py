6661967e549dca6aba7389d5e0140123456e74da
DaminikaDz <160649330+DaminikaDz@users.noreply.github.com>
2026-01-29 13:42:28 +0100

    Update conf_matrix_and_stat_test.py

--- codes/conf_matrix_and_stat_test.py
+++ codes/conf_matrix_and_stat_test.py
@@ -8,7 +8,6 @@ import seaborn as sns
 from sklearn.metrics import confusion_matrix
 from scipy.stats import chi2
 
-# Zakładamy, że klasy są w tych samych plikach co wcześniej
 from train_resnet1d import ResNet1D
 from data_ptbxl import make_dataloaders
 
@@ -20,7 +19,6 @@ def main():
     OUT_DIR = os.path.join(ART_DIR, "final_results")
     os.makedirs(OUT_DIR, exist_ok=True)
 
-    # 1. Wczytanie danych i modeli
     train_df = pd.read_csv(os.path.join(ART_DIR, "ptbxl_train.csv"))
     val_df = pd.read_csv(os.path.join(ART_DIR, "ptbxl_val.csv"))
     test_df = pd.read_csv(os.path.join(ART_DIR, "ptbxl_test.csv"))
@@ -30,20 +28,16 @@ def main():
         batch_size=64, use_derivative=True, use_augment=False
     )
 
-    # Model DL
     resnet = ResNet1D(in_channels=24, n_classes=5).to(device)
     resnet.load_state_dict(
         torch.load(os.path.join(ART_DIR, "resnet_augmented/resnet_aug_best.pt"), map_location=device))
     resnet.eval()
 
-    # Model Baseline
     rf_model = joblib.load(os.path.join(ART_DIR, "baseline/baseline_rf_model.pkl"))
     X_test_rf = np.load(os.path.join(ART_DIR, "baseline/X_test_final.npy"))
 
-    # 2. Zbieranie predykcji
     all_resnet_preds, all_rf_preds, all_true = [], [], []
 
-    # DL preds
     with torch.no_grad():
         for x, y in test_loader:
             all_resnet_preds.append(resnet(x.to(device)).argmax(1).cpu().numpy())
@@ -53,8 +47,7 @@ def main():
     y_true = np.concatenate(all_true)
     y_rf = rf_model.predict(X_test_rf)
 
-    # 3. Test McNemara (ResNet vs RF)
-    # b: ResNet dobrze, RF źle | c: ResNet źle, RF dobrze
+    #test McNemara (ResNet vs RF)
     b = np.sum((y_resnet == y_true) & (y_rf != y_true))
     c = np.sum((y_resnet != y_true) & (y_rf == y_true))
 
@@ -66,7 +59,7 @@ def main():
     if p_value < 0.05:
         print("Różnica jest istotna statystycznie (p < 0.05)")
 
-    # 4. Macierz Pomyłek dla najlepszego modelu
+    #macierz pomyłek dla najlepszego modelu
     class_names = ["NORM", "MI", "STTC", "CD", "HYP"]
     cm = confusion_matrix(y_true, y_resnet)
     cm_norm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
